57085f9d4f2de7bc5c04683acbe615e8a7a207e8
Andrew Fraser <a.vector@gmail.com>
2022-09-01 22:04:45 +0200

    fix existing test

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,9 +1,11 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+  it('should degrade a normal item with positive days and quality correctly', () => {
+    const gildedRose = new GildedRose([new Item('foo', 1, 1)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+    expect(items[0].name).toBe('foo');
+    expect(items[0].sellIn).toBe(0);
+    expect(items[0].quality).toBe(0);
   });
 });
