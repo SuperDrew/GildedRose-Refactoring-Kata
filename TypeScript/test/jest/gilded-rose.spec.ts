5db8628aa955a480f67c34b37eced60657f42426
Andrew Fraser <a.vector@gmail.com>
2022-09-01 22:07:30 +0200

    refactor extract method

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,18 +1,20 @@
-import { Item, GildedRose } from '@/gilded-rose';
+import {GildedRose, Item} from '@/gilded-rose';
+
+function checkItem(items: Array<Item>, expectedName: string, expectedSellIn: number, expectedQuality: number) {
+  expect(items[0].name).toBe(expectedName);
+  expect(items[0].sellIn).toBe(expectedSellIn);
+  expect(items[0].quality).toBe(expectedQuality);
+}
 
 describe('Gilded Rose', () => {
   it('should degrade a normal item with positive days and quality correctly', () => {
     const gildedRose = new GildedRose([new Item('foo', 1, 1)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('foo');
-    expect(items[0].sellIn).toBe(0);
-    expect(items[0].quality).toBe(0);
+    checkItem(items, 'foo', 0, 0);
   });
   it('should degrade a normal item past its SellIn days twice as fast', () => {
     const gildedRose = new GildedRose([new Item('foo', 0, 2)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('foo');
-    expect(items[0].sellIn).toBe(-1);
-    expect(items[0].quality).toBe(0);
+    checkItem(items, 'foo', -1, 0);
   })
 });
