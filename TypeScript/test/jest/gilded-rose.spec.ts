32c2cb392b51b6a3f06e06ea30a541bd4a7556f9
Andrew Fraser <a.vector@gmail.com>
2022-09-01 22:05:02 +0200

    add test for degrading a normal item twice as fast

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -8,4 +8,11 @@ describe('Gilded Rose', () => {
     expect(items[0].sellIn).toBe(0);
     expect(items[0].quality).toBe(0);
   });
+  it('should degrade a normal item past its SellIn days twice as fast', () => {
+    const gildedRose = new GildedRose([new Item('foo', 0, 2)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toBe('foo');
+    expect(items[0].sellIn).toBe(-1);
+    expect(items[0].quality).toBe(0);
+  })
 });
