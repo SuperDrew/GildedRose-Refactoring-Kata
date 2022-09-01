f8d9439816bbb9bcd5b2115882a5e46467941aa0
Andrew Fraser <a.vector@gmail.com>
2022-09-01 22:08:20 +0200

    refactor method sig to take an item rather than array

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,20 +1,20 @@
 import {GildedRose, Item} from '@/gilded-rose';
 
-function checkItem(items: Array<Item>, expectedName: string, expectedSellIn: number, expectedQuality: number) {
-  expect(items[0].name).toBe(expectedName);
-  expect(items[0].sellIn).toBe(expectedSellIn);
-  expect(items[0].quality).toBe(expectedQuality);
+function checkItem(item: Item, expectedName: string, expectedSellIn: number, expectedQuality: number) {
+  expect(item.name).toBe(expectedName);
+  expect(item.sellIn).toBe(expectedSellIn);
+  expect(item.quality).toBe(expectedQuality);
 }
 
 describe('Gilded Rose', () => {
   it('should degrade a normal item with positive days and quality correctly', () => {
     const gildedRose = new GildedRose([new Item('foo', 1, 1)]);
     const items = gildedRose.updateQuality();
-    checkItem(items, 'foo', 0, 0);
+    checkItem(items[0], 'foo', 0, 0);
   });
   it('should degrade a normal item past its SellIn days twice as fast', () => {
     const gildedRose = new GildedRose([new Item('foo', 0, 2)]);
     const items = gildedRose.updateQuality();
-    checkItem(items, 'foo', -1, 0);
+    checkItem(items[0], 'foo', -1, 0);
   })
 });
